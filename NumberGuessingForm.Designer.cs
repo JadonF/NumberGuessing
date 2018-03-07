c0556c4b49fa4cc28bfdc1efd985cbada1385f33
JadonF <jadon.fournier@ocsbstudent.ca>
2018-03-07 10:12:47 -0500

    Initial commit

---
+++ NumberGuessingForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace NumberGuessing
+{
+    partial class frmNumberGuessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblPrompt.Location = new System.Drawing.Point(13, 26);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(357, 26);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Guess a number between 1 and 10.";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(388, 31);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnCheck.Location = new System.Drawing.Point(48, 142);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(98, 37);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblAnswer.Location = new System.Drawing.Point(183, 148);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(85, 26);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "Answer";
+            // 
+            // frmNumberGuessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 261);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "frmNumberGuessing";
+            this.Text = "Number Guessing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ NumberGuessingForm.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by: Jadon Fournier
+ * Created on: 7-Mar-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #11 - NumberGuessing
+ * This program allows you to try and guess a hidden number.
+*/
+   
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuessing
+{
+    public partial class frmNumberGuessing : Form
+    {
+        // Set variable and constants
+        const double Number = 3;
+        double Input;
+
+        public frmNumberGuessing()
+        {
+            InitializeComponent();
+
+            //Hide labels
+            this.lblAnswer.Hide();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            Input = Convert.ToDouble(txtInput.Text);
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (Input == Number)
+            {
+                lblAnswer.Text = "That is correct!";
+            }
+
+            else
+            {
+                lblAnswer.Text = "Incorrect!";
+            }
+
+            this.lblAnswer.Show();
+        }
+    }
+}
